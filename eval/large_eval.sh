c8cc524dcbc809e2acdcfec75044e78b5432c78c
claraxfee <claraxfee@gmail.com>
2025-07-20 18:28:36 -0400

    large scale eval script

--- eval/large_eval.sh
+++ eval/large_eval.sh
@@ -2,5 +2,23 @@
 
 
 
+PROJECTS=(Chart Cli Closure Codec Collections Compress Csv Gson JacksonCore JacksonDatabind JacksonXml Jsoup JxPath Lang Math Mockito Time)
+
+for PROJECT in "${PROJECTS[@]}"; do
+
+	file=bugnums/"${PROJECT}".txt
+
+	while IFS= read -r BUG_ID; do
+
+		echo ""
+		echo ""		
+		echo "EVALUATING ${PROJECT} ${BUG_ID}"
+		./prefix_exception_eval.sh ${PROJECT} ${BUG_ID}
+		
+
+	done < "$file"
+done
+	
+
 
 
